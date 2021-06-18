c1f68246e9d835f19ec461f8dc06b34f616c8a56
Aaron Bentley <aaron@aaronbentley.com>
2021-06-18 18:02:32 -0400

    Support subcommands, --tree, index, relative paths.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::path::PathBuf;
 use std::process::{Command};
 use std::os::unix::process::CommandExt;
 use structopt::StructOpt;
@@ -6,6 +8,8 @@ use structopt::StructOpt;
 #[structopt()]
 enum Opt{
     Cat {
+        #[structopt(long, short, default_value="HEAD")]
+        tree: String,
         input: String,
     }
 }
@@ -18,15 +22,35 @@ fn make_git(command: &str) -> Command{
 }
 
 
-fn cat_cmd(input: &str) -> std::io::Error{
-    make_git("show").arg(format!(":{}", input)).exec()
+fn cat_cmd(input: &str, mut tree: &str) -> std::io::Error{
+    if tree == "index"{
+        tree = "";
+    }
+    make_git("show").arg(format!("{}:./{}", tree, input)).exec()
 }
 
 
 fn main() {
-    let opt = Opt::from_args();
+    let mut args_iter = env::args();
+    let progpath = PathBuf::from(args_iter.next().unwrap());
+    let progname = progpath.file_name().unwrap().to_str().unwrap();
+    let opt = match progname{
+        "nit" => Opt::from_args(),
+        _ => {
+            let mut args = vec!["nit".to_string()];
+            let mut subcmd_iter = progname.split('-');
+            subcmd_iter.next();
+            for arg in subcmd_iter{
+                args.push(arg.to_string());
+            }
+            for arg in args_iter{
+                args.push(arg);
+            }
+            Opt::from_iter(args)
+        }
+    };
     let result = match opt {
-        Opt::Cat{input} => cat_cmd(&input)
+        Opt::Cat{input, tree} => cat_cmd(&input, &tree),
     };
     println!("{:?}", result);
 }

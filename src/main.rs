3bcc884ccf8418faf0fe4f70cc77e69a2fecb222
Aaron Bentley <aaron@aaronbentley.com>
2021-06-18 22:16:36 -0400

    Extract some command parsing.

--- src/main.rs
+++ src/main.rs
@@ -30,12 +30,9 @@ fn cat_cmd(input: &str, mut tree: &str) -> std::io::Error{
 }
 
 
-fn main() {
-    let mut args_iter = env::args();
-    let progpath = PathBuf::from(args_iter.next().unwrap());
-    let args_vec:Vec<String> = args_iter.collect();
+fn parse_args(progpath: &PathBuf, args_vec: &Vec<String>) -> Result<Opt, clap::Error>{
     let progname = progpath.file_name().unwrap().to_str().unwrap();
-    let opt = match progname{
+    match progname{
         "nit" => {
             Opt::from_args_safe()
         },
@@ -46,19 +43,27 @@ fn main() {
             for arg in subcmd_iter{
                 args.push(arg.to_string());
             }
-            for arg in &args_vec{
+            for arg in args_vec{
                 args.push(arg.to_string());
             }
             Ok(Opt::from_iter(args))
         }
-    };
+    }
+}
+
+
+fn main() {
+    let mut args_iter = env::args();
+    let progpath = PathBuf::from(args_iter.next().unwrap());
+    let args_vec:Vec<String> = args_iter.collect();
+    let opt = parse_args(&progpath, &args_vec);
     match opt {
         Ok(Opt::Cat{input, tree}) => cat_cmd(&input, &tree),
         Err(err) => {
             if err.kind == clap::ErrorKind::UnknownArgument {
                 let mut git_args = vec![];
-                for git_arg in args_vec{
-                    git_args.push(git_arg);
+                for git_arg in &args_vec{
+                    git_args.push(git_arg.clone());
                 }
                 Command::new("git").args(git_args).exec()
             }

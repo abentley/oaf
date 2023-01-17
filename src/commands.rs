d14ff8a90011b6769d62ba2035dbdb38f1cc11a0
Aaron Bentley <aaron@aaronbentley.com>
2023-01-17 00:38:29 -0500

    Implement pipeline command properly.

--- src/commands.rs
+++ src/commands.rs
@@ -977,6 +977,7 @@ impl Runnable for NextBranch {
     }
 }
 
+/// List a branch sequence
 #[derive(Debug, Args)]
 pub struct Pipeline {}
 impl Runnable for Pipeline {
@@ -995,27 +996,54 @@ impl Runnable for Pipeline {
             }
             Ok(current) => current,
         };
+        let mut previous = vec![];
         let mut current = RefName::from_long(current_lb.full().into()).find_shorthand(&repo);
         loop {
-            println!("{}", current.get_shortest());
-            let Ok(BranchName::Local(current_lb)) = BranchName::from_str(current.get_longest()) else {
+            let Ok(BranchName::Local(current_lb)) =
+                    BranchName::from_str(current.get_longest()) else {
                 eprintln!("Not a local branch");
                 return 1
             };
-            current = match advance(&repo, current_lb) {
+            current = match advance::<PipePrev>(&repo, current_lb) {
+                Err(_) => {
+                    eprintln!("Error!");
+                    return 1;
+                }
+                Ok(Some(current)) => current,
+                Ok(None) => break,
+            };
+            previous.push(current.get_shortest().to_owned());
+        }
+        previous.reverse();
+        for branch in previous {
+            println!("  {}", branch);
+        }
+        current = RefName::from_long(current_lb.full().into()).find_shorthand(&repo);
+        println!("* {}", current.get_shortest());
+        loop {
+            let Ok(BranchName::Local(current_lb)) =
+                    BranchName::from_str(current.get_longest()) else {
+                eprintln!("Not a local branch");
+                return 1
+            };
+            current = match advance::<PipeNext>(&repo, current_lb) {
                 Err(_) => {
                     eprintln!("Error!");
                     return 1;
                 }
                 Ok(Some(current)) => current,
                 Ok(None) => break 0,
-            }
+            };
+            println!("  {}", current.get_shortest());
         }
     }
 }
 
-fn advance(repo: &Repository, current_lb: LocalBranchName) -> Result<Option<RefName>, RefErr> {
-    match resolve_symbolic_reference(repo, &PipeNext::from(current_lb)) {
+fn advance<T: SiblingBranch + From<LocalBranchName> + ReferenceSpec>(
+    repo: &Repository,
+    current_lb: LocalBranchName,
+) -> Result<Option<RefName>, RefErr> {
+    match resolve_symbolic_reference(repo, &T::from(current_lb)) {
         Ok(next) => Ok(Some(RefName::from_long(next).find_shorthand(repo))),
         Err(RefErr::NotFound(_)) => Ok(None),
         Err(err) => Err(err),

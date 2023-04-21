aa6f7e7a735607847a43d62143a757753a2366ef
Aaron Bentley <aaron@aaronbentley.com>
2023-04-21 13:21:32 -0400

    Refactor tests.

--- tests/insert_branch.rs
+++ tests/insert_branch.rs
@@ -30,3 +30,18 @@ fn insert_prev() {
     assert!(bar.find_reference(&repo).is_ok());
     assert!(bar.inverse().find_reference(&repo).is_err());
 }
+
+#[test]
+fn insert_next_twice() {
+    let work_dir = common::init_blank_repo();
+    let repo = Repository::open(work_dir).unwrap();
+    let foo = PipeNext::from(LocalBranchName::from("foo".to_string()));
+    let bar = LocalBranchName::from("bar".to_string());
+    let (foo, bar) = foo.insert_branch(&repo, bar).unwrap();
+    let baz = LocalBranchName::from("baz".to_string());
+    let (bar, baz) = bar.inverse().insert_branch(&repo, baz).unwrap();
+    assert!(foo.find_reference(&repo).is_ok());
+    assert!(foo.inverse().find_reference(&repo).is_err());
+    assert!(bar.find_reference(&repo).is_ok());
+    assert!(bar.inverse().find_reference(&repo).is_err());
+}
